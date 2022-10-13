a5ce1b5516fe53fa7fad61ec6c675ee09662bc18
Ahmed Muse <ahmeddove114@gmail.com>
2022-10-13 22:33:08 +0300

    first commit

---
+++ allifmainapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def allifmaalmaindashboard(request):
+    context={
+
+    }
+    return render(request,'allifmainapp/dashboard/allifmaaldashboard.html',context)
